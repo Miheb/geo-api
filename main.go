c9df0b2202164f4e703bc5cc83bf0f5c51e7435d
William Weill <wweill84@gitlab.com>
2019-02-13 12:32:08 +0100

    Avoid conflicts when two gateways share the same lat, long or place.

--- main.go
+++ main.go
@@ -43,18 +43,38 @@ func inter3 (g1, g2, g3 swagger.GatewayReceptionTdoa) swagger.LocationEstimate {
 	CX3 := 2*(g3.AntennaLocation.Latitude - g1.AntennaLocation.Latitude)
 	CY2 := 2*(g2.AntennaLocation.Longitude - g1.AntennaLocation.Longitude)
 	CY3 := 2*(g3.AntennaLocation.Longitude - g1.AntennaLocation.Longitude)
-
-	if isEqualPlace(g1,g2) || isEqualPlace(g1,g3)
-
-
 	CR2 := math.Pow(g1.Rssi, 2) - math.Pow(g2.Rssi, 2) + (math.Pow(g2.AntennaLocation.Latitude,2) + math.Pow(g2.AntennaLocation.Longitude,2)) - (math.Pow(g1.AntennaLocation.Latitude,2) + math.Pow(g1.AntennaLocation.Longitude,2))
 	CR3 := math.Pow(g1.Rssi, 2) - math.Pow(g3.Rssi, 2) + (math.Pow(g3.AntennaLocation.Latitude,2) + math.Pow(g3.AntennaLocation.Longitude,2)) - (math.Pow(g1.AntennaLocation.Latitude,2) + math.Pow(g1.AntennaLocation.Longitude,2))
 
-	CYnum := CR2 - CX2 * CR3 / CX3
-	CYden := CY2 - CX2 * CY3 / CX3
+	var CX float64
+	var CY float64
 
-	CY := CYnum / CYden
-	CX := (CR3 - CY * CY3) / CX3
+	if isEqualPlace(g1,g2) || isEqualPlace(g1,g3) {
+		fmt.Println("The three gateways are not distinct")
+		return swagger.LocationEstimate{0, 0, 0, 0}
+	} else if isEqualLat(g1,g2) {
+		// 1 et 2 même x
+		CY = CR2 / CY2
+		CX = (CR3 - CY * CY3) / CX3
+	} else if isEqualLat(g1,g3) {
+		// 1 et 3 même x
+		CY = CR3 / CY3
+		CX = (CR2 - CY * CY2) / CX2
+	} else if isEqualLong(g1,g2) {
+		// 1 et 2 même y
+		CX = CR2 / CX2
+		CY = (CR3 - CX * CX3) / CY3
+	} else if isEqualLong(g1,g3) {
+		// 1 et 3 même y
+		CX = CR3 / CX3
+		CY = (CR2 - CX * CX2) / CY2
+	} else {
+		CYnum := CR2 - CX2 * CR3 / CX3
+		CYden := CY2 - CX2 * CY3 / CX3
+
+		CY = CYnum / CYden
+		CX = (CR3 - CY * CY3) / CX3
+		}
 
 	return swagger.LocationEstimate{CX, CY, 0, 0}
 }
@@ -70,8 +90,8 @@ func main() {
     "toa": 0,
     "encryptedToa": "",
     "antennaLocation": {
-      "latitude": 2,
-      "longitude": 2,
+      "latitude": 4,
+      "longitude": 0,
       "altitude": 0
 		}
 	},
@@ -83,8 +103,8 @@ func main() {
     "toa": 0,
     "encryptedToa": "",
     "antennaLocation": {
-      "latitude": 4,
-      "longitude": 0,
+      "latitude": 2,
+      "longitude": 2,
       "altitude": 0
 		}
 	},{
